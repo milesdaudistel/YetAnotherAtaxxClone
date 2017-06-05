a86d449097868880d9947a60598d7cbdb782122f
milesdaudistel <miles.daudistel@berkeley.edu>
2017-06-05 11:58:36 -0700

    jebaited

--- src/GUI.java
+++ src/GUI.java
@@ -27,71 +27,54 @@ public class GUI extends JPanel implements MouseListener {
 
     private int side;
 
+    private int piece_diameter;
+
     public GUI(int board_length) {
         super(new GridLayout(board_length, board_length));
 
         side = board_length;
         from = null;
         board = new Board(board_length);
+        addMouseListener(this);
 
     }
 
 
-    static void make_GUI(int n) {
+
+
+    static void make_GUI(int n, int width) {
         //pack lets the layout manager take care of sizing
         //you can either setpreferredsize and then pack, or explicity setsize and dont pack
         JFrame jframe = new JFrame("Ataxx");
-        jframe.setPreferredSize(new Dimension(400, 400));
+        jframe.setPreferredSize(new Dimension(width, width));
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         GUI gui = new GUI(n);
         jframe.getContentPane().add(gui);
         jframe.setResizable(false);
         jframe.pack();
-        //gui.scale_board_image();
+        gui.init_piece_diameter();
         jframe.setVisible(true);
     }
 
-    void scale_board_image() {
-        try {
-            int height = this.getHeight();
-            int width = this.getWidth();
-            System.out.println("ok");
-            System.out.println(height);
-            red = ImageIO.read(new File("images/red.png"));
-            System.out.println(red.getHeight());
-            red = (BufferedImage) red.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-
-
-            blue = ImageIO.read(new File("images/blue.png"));
-            blue = (BufferedImage) blue.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-
-            empty = ImageIO.read(new File("images/empty.png"));
-            empty = (BufferedImage) empty.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-
-        } catch (IOException e) {
-            //Todo kill self
-            System.out.println(e);
-            return;
-        }
-
-        repaint();
-    }
     public void mousePressed(MouseEvent e) {
         Coordinate clicked = location_to_index(e.getX(), e.getY());
         int x = clicked.x;
         int y = clicked.y;
+        System.out.println("x: " + x + " y: " + y);
         
         if (from == null && board.get(x, y) == board.whoseturn()) {
             from = clicked;
+            repaint();
         } else if (from != null && board.get(x, y) == Piece.EMPTY) {
             board.update(new Move(from, clicked));
             from = null;
+            repaint();
         } else if (from == clicked) {
             from = null;
+            repaint();
         }
 
-        repaint();
     }
 
     public void mouseReleased(MouseEvent e) {}
@@ -104,40 +87,52 @@ public class GUI extends JPanel implements MouseListener {
 
     public void paintComponent(Graphics g) {
         //g.drawImage(red, 0, 0, this);
-        g.drawOval(0, 0, 50, 50);
+        //g.drawOval(0, 0, 50, 50);
         paint_grid(g);
+        paint_pieces(g);
     }
 
-    public void paint_grid(Graphics g) {
+    void paint_grid(Graphics g) {
         //number of horizontal/vertical lines to draw is side-1
         for (int i = 0; i < side; i++) {
             int temp = (this.getWidth() * i) / side;
+            g.setColor(Color.black);
             g.drawLine(temp, 0, temp, this.getWidth());
             g.drawLine(0, temp, this.getHeight(), temp);
         }
     }
 
-    public void paint_pieces(Graphics g) {
-
-    }
-/*
-    void update(Board board) {
-        for (int i = 0; i < side; i++) {
-            for (int j = 0; j < side; j++) {
-                if (board.get(i, j) == Piece.EMPTY) {
-                    button_grid[i][j].setIcon(empty);
-                } else if (board.get(i, j) == Piece.RED) {
-                    button_grid[i][j].setIcon(red);
-                } else {
-                    button_grid[i][j].setIcon(blue);
+    void paint_pieces(Graphics g) {
+        for (int j = 0; j < side; j++) {
+            for (int i = 0; i < side; i++) {
+                if (board.get(i, j) == Piece.RED) {
+                    g.setColor(Color.red);
+                    Coordinate location = index_to_location(i, j);
+                    g.fillOval(location.x, location.y, piece_diameter, piece_diameter);
+                } else if (board.get(i, j) == Piece.BLUE) {
+                    g.setColor(Color.blue);
+                    Coordinate location = index_to_location(i, j);
+                    g.fillOval(location.x, location.y, piece_diameter, piece_diameter);
+                } else if (board.get(i, j) == Piece.EMPTY) {
+                    g.setColor(Color.white);
+                    Coordinate location = index_to_location(i, j);
+                    g.fillOval(location.x, location.y, piece_diameter, piece_diameter);
                 }
             }
         }
-
+        if (from != null) {
+            g.setColor(Color.gray);
+            Coordinate location = index_to_location(from.x, from.y);
+            g.fillOval(location.x, location.y, piece_diameter, piece_diameter);
+        }
     }
-*/
+
+    void init_piece_diameter() {
+        piece_diameter = this.getWidth() / side;
+    }
+
     Coordinate location_to_index(int x, int y) {
-        return new Coordinate(x / side, y / side);
+        return new Coordinate(x / piece_diameter, y / piece_diameter);
     }
 
     Coordinate index_to_location(int x, int y) {
